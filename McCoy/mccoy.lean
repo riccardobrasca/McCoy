d5cc7a0a8fff5bba2ba9afd3a7c7af56768e7b38
Riccardo Brasca <riccardo.brasca@gmail.com>
2024-01-30 11:44:52 +0100

    statement

--- McCoy/mccoy.lean
+++ McCoy/mccoy.lean
@@ -1,3 +1,8 @@
 import Mathlib
 
-example : 1 = 1 := rfl
+open Polynomial nonZeroDivisors
+
+variable (R : Type*) [CommRing R] (P : R[X])
+
+theorem McCoy : P ∉ R[X]⁰ ↔ ∃ (a : R), a • P = 0 := by
+  sorry
