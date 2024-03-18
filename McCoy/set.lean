eb431f058c800e2e3c25da24798e098cb60abc2e
Manel Sadi <manelsadi12@gmail.com>
2024-03-18 14:07:46 +0100

    modifier

--- McCoy/set.lean
+++ McCoy/set.lean
@@ -1,5 +1,13 @@
 import Mathlib
 
-example (T : Type*) (A B C : Set T) (h1 : A ∪ B ⊆ B ∪ C) (h2 : A ∩ B ⊆ B ∩ C) :
+open Set
+
+example (T : Type*) (A B C : Set T) (h1 : A ∪ C ⊆ B ∪ C) (h2 : A ∩ C ⊆ B ∩ C) :
     A ⊆ B := by
-  sorry
+  intro x hx
+  have h4 : A ⊆ A ∪ C := by
+    exact fun ⦃a⦄ a_1 ↦ Or.inl a_1
+  cases' h1 (h4 hx) with a b
+  · exact a
+  · have c := And.intro (hx) (b)
+    exact mem_of_mem_inter_left (h2 c)
