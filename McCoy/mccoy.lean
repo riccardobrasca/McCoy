cb378fa5edde83f2f3e60c28179b69ec1a7c50dc
Riccardo Brasca <riccardo.brasca@gmail.com>
2024-05-14 15:29:13 +0200

    golf

--- McCoy/mccoy.lean
+++ McCoy/mccoy.lean
@@ -2,66 +2,33 @@ import Mathlib
 
 open Polynomial nonZeroDivisors BigOperators
 
-variable {R : Type* } [CommRing R] (P : R[X])
+variable {R : Type* } [CommSemiring R] (P : R[X])
 
 def Ann := {Q | Q * P= 0 ∧ Q ≠ 0}
 
 lemma Ann_nonvide (h : P ∉ R[X]⁰) : Ann P ≠ ∅ := by
   intro hvide
-  apply h
-  rw [mem_nonZeroDivisors_iff]
-  intro Q hQ
+  refine h (fun Q hQ => ?_)
   by_contra! hQ0
-  have hQmem : Q ∈ Ann P := by
-    dsimp [Ann]
-    constructor
-    · exact hQ
-    · exact hQ0
+  have hQmem : Q ∈ Ann P := ⟨hQ, hQ0⟩
   rw [hvide] at hQmem
   exact hQmem
-
-def Anndeg := natDegree '' (Ann P)
-
-lemma Anndeg_nonvide (h : P ∉ R[X]⁰) : Anndeg P ≠ ∅ := by
-  dsimp [Anndeg]
-  simp
-  exact Ann_nonvide P h
+lemma Anndeg_nonvide (h : P ∉ R[X]⁰) : natDegree '' (Ann P) ≠ ∅ := by
+  simp [Ann_nonvide P h]
 
 noncomputable
-def m := sInf (Anndeg P)
-
-lemma Lemma1 {Q : R[X]} (h : Q ∈ Ann P) : m P ≤ natDegree Q := by
-  dsimp[m]
-  exact Nat.sInf_le (Exists.intro Q { left := h, right := rfl })
-
-lemma Lemma2 (h : P ∉ R[X]⁰) : ∃ Q ∈ Ann P, natDegree Q = m P := by
-  dsimp[m]
-  have hnonvide : Set.Nonempty (Anndeg P) := by
-    have := Anndeg_nonvide P h
-    exact Set.nmem_singleton_empty.mp this
-  have := Nat.sInf_mem hnonvide
-  dsimp [Anndeg] at this
-  simp at this
-  exact this
+def m := sInf (natDegree '' (Ann P))
 
 lemma Lemma3 (h : P ∉ R[X]⁰) (h' : m P = 0 ) : ∃ (a : R), a ≠ 0 ∧ a • P = 0 := by
-  have := Lemma2 P h
+  have : ∃ Q ∈ Ann P, natDegree Q = m P := Nat.sInf_mem (Set.nmem_singleton_empty.mp (Anndeg_nonvide P h))
   rw [h'] at this
   dsimp [Ann] at this
   cases' this with Q hQ
-  have h'Q : ∃ (x : R), Polynomial.C x = Q := by
+  have ⟨x, hx⟩ : ∃ (x : R), Polynomial.C x = Q := by
    rw [Polynomial.natDegree_eq_zero] at hQ
    exact hQ.right
-  cases' h'Q with x hx
-  symm at hx
-  rw [hx] at hQ
-  rw [<-Polynomial.smul_eq_C_mul] at hQ
-  simp
-  rw [C_eq_zero] at hQ
-  use x
-  constructor
-  · exact hQ.1.2
-  · exact hQ.1.1
+  rw [← hx, ← Polynomial.smul_eq_C_mul, C_eq_zero] at hQ
+  exact ⟨x, hQ.1.2, hQ.1.1⟩
 
 lemma Lemma4 (h' : m P ≠ 0) (Q : R[X]) (hQ : Q ∈ Ann P) :
     ∃ i, Q.leadingCoeff * P.coeff i ≠ 0 := by
@@ -76,18 +43,12 @@ lemma Lemma4 (h' : m P ≠ 0) (Q : R[X]) (hQ : Q ∈ Ann P) :
     · simp
       dsimp[Ann] at hQ
       exact hQ.2
-  apply h'
   have hm : m P ≤ 0 := by
     dsimp[m]
     simp
     left
-    dsimp[Anndeg]
-    simp
-    use C ( leadingCoeff Q)
-    constructor
-    · assumption
-    · simp
-  exact Nat.le_zero.mp hm
+    exact ⟨C ( leadingCoeff Q), ‹_›, by simp⟩
+  exact h' <| Nat.le_zero.mp hm
 
 lemma Lemma5 (h' : m P ≠ 0) (Q : R[X]) (hQ : Q ∈ Ann P) :
     ∃ i, P.coeff i • Q ≠ 0 := by
@@ -110,10 +71,7 @@ lemma Lemma6 (Q : R[X]) (hQ : Q ∈ Ann P) : (P.coeff (l P Q) • Q) * P = 0 := 
 lemma Lemma7 (h' : m P ≠ 0) (Q : R[X]) (hQ : Q ∈ Ann P) :
     P.coeff (l P Q) • Q ≠ 0 := by
   have : l P Q ∈ { i | P.coeff i • Q ≠ 0 } := by
-    apply Nat.sSup_mem
-    · obtain ⟨i, hi⟩ := Lemma5 P h' Q hQ
-      use i
-      exact hi
+    apply Nat.sSup_mem (Lemma5 P h' Q hQ)
     · use P.natDegree
       intro i hi
       by_contra! H
@@ -151,7 +109,8 @@ lemma Lemma11 (Q : R[X]) (hmQ : natDegree Q = m P) :
   have h3 : coeff P (l P Q) * coeff Q (l P Q + m P - l P Q) = coeff P (l P Q) • leadingCoeff Q := by
     simp [leadingCoeff, hmQ]
   rw [h3, add_comm _ (coeff P (l P Q) • leadingCoeff Q), add_assoc]
-  simp only [smul_eq_mul, add_right_eq_self]
+  simp only [smul_eq_mul]
+  nth_rewrite 2 [← add_zero (coeff P (l P Q) * leadingCoeff Q)]
   rw [← zero_add (0 : R)]
   congr
   · apply Finset.sum_eq_zero
@@ -161,10 +120,7 @@ lemma Lemma11 (Q : R[X]) (hmQ : natDegree Q = m P) :
     rw [add_comm, Nat.add_sub_assoc hk.le]
     simp only [lt_add_iff_pos_right, tsub_pos_iff_lt]
     exact hk
-  · apply Finset.sum_eq_zero
-    intro k _
-    apply h1
-    linarith
+  · exact Finset.sum_eq_zero (fun k _ ↦ h1 _ _ (by omega))
 
 lemma Lemma8 (Q : R[X]) (hQ : Q ∈ Ann P) (hmQ : natDegree Q = m P) :
     P.coeff (l P Q) •  Q.leadingCoeff = 0 := by
@@ -173,45 +129,22 @@ lemma Lemma8 (Q : R[X]) (hQ : Q ∈ Ann P) (hmQ : natDegree Q = m P) :
 
 lemma Lemma9 (h' : m P ≠ 0) (Q : R[X]) (hQ : Q ∈ Ann P) (hmQ : natDegree Q = m P) :
     natDegree (P.coeff (l P Q) • Q) < natDegree Q := by
-  apply lt_of_le_of_ne
-  · exact natDegree_smul_le (coeff P (l P Q)) Q
-  · intro h
-    have := Lemma8 P Q hQ hmQ
-    rw [← coeff_natDegree, ← h, ← coeff_smul, coeff_natDegree, leadingCoeff_eq_zero] at this
-    apply Lemma7 P h' Q hQ
-    exact this
+  refine lt_of_le_of_ne (natDegree_smul_le (coeff P (l P Q)) Q) (fun h ↦ ?_)
+  have := Lemma8 P Q hQ hmQ
+  rw [← coeff_natDegree, ← h, ← coeff_smul, coeff_natDegree, leadingCoeff_eq_zero] at this
+  exact Lemma7 P h' Q hQ this
 
 theorem McCoy : P ∉ R[X]⁰ ↔ ∃ (a : R), a ≠ 0 ∧ a • P = 0 := by
-  constructor
-  · intro h
-    apply  Lemma3 P h
-    by_contra! Hm
-    cases' Lemma2 P h with Q hQ
-    cases' hQ with hQP hQdeg
-    let Q' := P.coeff (l P Q) • Q
-    have hQ' : Q' ∈ Ann P := by
-      dsimp [Ann]
-      constructor
-      · exact Lemma6 P Q hQP
-      · exact Lemma7 P Hm Q hQP
-    have key : natDegree Q' < natDegree Q := by exact Lemma9 P Hm Q hQP hQdeg
-    rw [hQdeg] at key
-    have hdegQ' : natDegree Q' ∈ Anndeg P := by
-      dsimp [Anndeg]
-      use Q'
-    rw [lt_iff_not_le] at key
-    apply key
-    dsimp [m]
-    exact Nat.sInf_le hdegQ'
-  · intro h h1
-    cases' h with a h3
-    cases' h3 with h4 h5
-    rw [mem_nonZeroDivisors_iff] at h1
-    specialize h1 (C a)
-    have h6 : a • P = ( C a ) * P := by
-      exact smul_eq_C_mul a
-    rw [h6] at h5
-    have h7:= h1 h5
-    have h8 : a = 0 := by
-      exact C_eq_zero.mp h7
-    exact h4 h8
+  refine ⟨fun h ↦ Lemma3 P h ?_,
+    fun ⟨a, ha, h⟩ h1 ↦ ha <| C_eq_zero.1 <| (h1 (C a)) <| smul_eq_C_mul a ▸ h⟩
+  by_contra! Hm
+  cases' Nat.sInf_mem (Set.nmem_singleton_empty.mp (Anndeg_nonvide P h)) with Q hQ
+  cases' hQ with hQP hQdeg
+  let Q' := P.coeff (l P Q) • Q
+  have hQ' : Q' ∈ Ann P := ⟨Lemma6 P Q hQP, Lemma7 P Hm Q hQP⟩
+  have key : natDegree Q' < natDegree Q := by exact Lemma9 P Hm Q hQP hQdeg
+  rw [hQdeg] at key
+  have hdegQ' : natDegree Q' ∈ natDegree '' (Ann P) := by
+    use Q'
+  rw [lt_iff_not_le] at key
+  exact key <| Nat.sInf_le hdegQ'
